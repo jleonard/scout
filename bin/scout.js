af672cc68edace327eba6187e0ec90afeaf286cb
jleonard <thejohnleonard@gmail.com>
2014-06-10 20:58:34 -0400

    recursive mkdir

--- bin/scout.js
+++ bin/scout.js
@@ -4,42 +4,42 @@ var findup = require('findup-sync');
 var path   = require('path');
 var cwd    = process.cwd();
 var shell  = require('child_process').exec;
+var mkdirp = require('mkdirp');
 var json;
 
 var userArguments = process.argv.slice(2);
 
 var configFile = findup('scoutfile.json', { cwd: cwd });
 if (configFile) {
-  //console.log('Found scoutfile:', configFile);
   json = JSON.parse(fs.readFileSync(configFile));
-  pre();
+  hooks('pre');
   makeFolders();
+  hooks('post');
   cwd = path.dirname(configFile);
   process.chdir(cwd);
-  //console.log('Setting current working directory:', cwd);
 } else {
   console.log('No scoutfile found.');
   process.exit(1);
 }
 
-function pre(){
+function hooks(hook){
   function firstKey(cur){
     for(var a in cur){ return a};
   }
-  var arr = json.hasOwnProperty('hooks') && json.hooks.hasOwnProperty('pre') ? json.hooks.pre : [];
-  console.log(arr);
+  var arr = json.hasOwnProperty("hooks") && json.hooks.hasOwnProperty(hook) ? json["hooks"][hook] : [];
   var len = arr.length;
+  if(len > 0){
+    console.log('Running '+hook+' hook...');
+  }
   for(var ii = 0; ii < len; ii++){
     var cur = arr[ii];
-
     var c = typeof(cur) === 'string' ? cur : firstKey(cur);
-
-    console.log('command = ',c,typeof(c));
-
     var spawn = shell(cur,[]);
+    
     spawn.stdout.on('data', function (data) {
-      console.log('data ',data);
+      console.log(data);
     });
+
     spawn.stderr.on('data', function (data) {
       console.log('ERROR: ' + data);
     });
@@ -53,18 +53,22 @@ function makeFolders(){
   var len = arr.length;
   for(var ii = 0; ii < len; ii++){
     var cur = arr[ii];
-    console.log('ding ',cur);
     if(fs.existsSync(cur)){
       console.log('The directory ',cur, ' already exists.');
     }else{
+      mkdirp(cur, function (err) {
+        if (err) console.error(err)
+        else console.log('pow!')
+      });
+      /*
       fs.mkdir(cur,function(e){
         if(e){
           console.log('error ',e);
         }else{
-          console.log('created directory: ',e);
+          console.log('created directory: ',cur);
         }
       });
+    */
     }
-    
   }
 }
